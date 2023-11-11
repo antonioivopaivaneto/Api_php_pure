47a7068925dd00d0470281f92ebae60ad3d6bb1b
junior <antonioivo.3@gmail.com>
2023-11-11 12:39:49 -0300

    api finally

---
+++ php_api/api_core/data.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'name' => 'joao',
+        'email' => 'joao@gmail.com',
+    ],
+    [
+        'name' => 'maria',
+        'email' => 'maria@gmail.com',
+    ],
+    [
+        'name' => 'thais',
+        'email' => 'joao@outlook.com',
+    ],
+    [
+        'name' => 'clara',
+        'email' => 'clara@hotmail.com',
+    ],
+    [
+        'name' => 'helena',
+        'email' => '',
+    ]
+];
